5d2791caef5ca59980ba77bd2b795f3f2bd3026b
Jacob Cieply <114676624+ciepme@users.noreply.github.com>
2026-02-04 20:55:51 -0500

    Add LC Addon

--- matlab_for_arduino/BiteBotArduino.m
+++ matlab_for_arduino/BiteBotArduino.m
@@ -1,26 +1,44 @@
 classdef BiteBotArduino
     properties
         arduino_object
+        latch
+        loadcell
         press_reg_pin
         press_valve_pin
         press_pump_pin
         vacuum_valve_pin
+        lc_data_pin
+        lc_clock_pin
+        latch_pin
     end
     
     methods
-        function obj = BiteBotArduino(a,press_reg_pin,press_valve_pin,press_pump_pin,vacuum_valve_pin)
+        function obj = BiteBotArduino(a, press_reg_pin, press_valve_pin, ...
+                press_pump_pin, vacuum_valve_pin, lc_data_pin, lc_clock_pin, ...
+                latch_pin)
+            % Set Object Parameters
             obj.arduino_object = a;
             obj.press_reg_pin = press_reg_pin;
             obj.press_valve_pin = press_valve_pin;
             obj.press_pump_pin = press_pump_pin;
             obj.vacuum_valve_pin = vacuum_valve_pin;
+            obj.lc_data_pin = lc_data_pin;
+            obj.lc_clock_pin = lc_clock_pin;
+            obj.latch_pin = latch_pin;
 
+            % Configure inputs and outputs
+            configurePin(obj.arduino_object, obj.press_reg_pin, "DigitalOutput");
+            configurePin(obj.arduino_object, obj.press_valve_pin, "DigitalOutput");
+            configurePin(obj.arduino_object, obj.press_pump_pin, "DigitalOutput");
+            configurePin(obj.arduino_object, obj.vacuum_valve_pin, "DigitalOutput");
+            obj.latch = servo(obj.arduino_object, obj.latch_pin);
+            obj.loadcell = addon(obj.arduino_object, 'basicHX711/basic_HX711', ...
+                {'D3', 'D2'});
         end
-        function activate(obj)
-            configurePin(obj.arduino_object, obj.blink_pin, "DigitalOutput");
-            writeDigitalPin(obj.arduino_object, obj.blink_pin, 1);
+        function blink(obj)
+            writeDigitalPin(obj.arduino_object, obj.vacuum_valve_pin, 1);
             pause(3);
-            writeDigitalPin(obj.arduino_object, obj.blink_pin, 0);
+            writeDigitalPin(obj.arduino_object, obj.vacuum_valve_pin, 0);
         end
     end
 end
--- matlab_for_arduino/bitebot_arduino_sandbox_2.m
+++ matlab_for_arduino/bitebot_arduino_sandbox_2.m
@@ -1,7 +1,16 @@
+%clear workspace
+clear;
+
 % create variables
 a = arduino;
 press_reg_pin = 'D4';
 press_valve_pin = 'D7';
 press_pump_pin = 'D8';
 vacuum_valve_pin = 'D12';
-biteBot = BiteBotArduino(a,press_reg_pin,press_valve_pin,press_pump_pin,vacuum_valve_pin);
+lc_data_pin = "D3";
+lc_clock_pin = "D2";
+latch_pin = "A0";
+biteBot = BiteBotArduino(a, press_reg_pin, press_valve_pin, ...
+    press_pump_pin, vacuum_valve_pin, lc_data_pin, lc_clock_pin, ...
+    latch_pin);
+biteBot.blink();
