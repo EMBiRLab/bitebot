eee53d399985bc8e31e0e9ab56d993311cb78c6f
Jacob Cieply <114676624+ciepme@users.noreply.github.com>
2026-02-07 15:43:39 -0500

    Load Cell Working
    transposed pins

--- matlab_for_arduino/bitebot_arduino_sandbox_2.m
+++ matlab_for_arduino/bitebot_arduino_sandbox_2.m
@@ -1,7 +1,9 @@
+%uses bitebot class for everything except LC
+
 %clear workspace
 clear;
 
-% create variables
+%% create variables
 a = arduino;
 press_reg_pin = 'D4';
 press_valve_pin = 'D7';
@@ -14,3 +16,17 @@ biteBot = BiteBotArduino(a, press_reg_pin, press_valve_pin, ...
     press_pump_pin, vacuum_valve_pin, lc_data_pin, lc_clock_pin, ...
     latch_pin);
 biteBot.blink();
+
+% create LC in sandbox
+lc_pins = {char(lc_data_pin), char(lc_clock_pin)};
+loadCell = addon(biteBot.arduino_object, 'basicHX711/basic_HX711', lc_pins');
+
+%%
+input('Clear load cell then press enter to continue...', 's');
+cal = calibration(50, 500);
+cal.tare_weight = tare(cal, loadCell);
+input('Add 500g mass then press enter to continue...', 's');
+cal.scale_factor = scale(cal,loadCell);
+cal_weight = get_weight(cal,loadCell);
+%disp(["Mass of ", cal_weight, "g detected."]);
+input('Remove 500g mass then press enter to continue...', 's');
